cc1df17488c9eff1a883b9bf42cafb92113cb12a
Marcus Camargo <marcus@agenciavisia.com.br>
2020-01-23 16:13:42 -0300

    Add Image to Dashboard

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -9,6 +9,21 @@ class Dashboard extends StatelessWidget {
       appBar: AppBar(
         title: Text(_tituloTela),
       ),
+      body: Column(
+        children: <Widget>[
+          Image.asset('images/bytebank_logo.png'),
+          Container(
+            height: 120,
+            color: Colors.green,
+            child: Column(
+              children: <Widget>[
+                Icon(Icons.people),
+                Text('Contacts'),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
