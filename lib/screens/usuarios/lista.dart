4d5213d196dd1f533fc811d06c55f56cf0c72efc
Marcus Camargo <marcus@agenciavisia.com.br>
2020-01-27 10:44:22 -0300

    User list screen

---
+++ lib/screens/usuarios/lista.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+const _tituloAppBar = 'Contacts';
+
+class Contatos extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_tituloAppBar),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text(
+                'Gabbs',
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+              subtitle: Text(
+                '1000',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add,),
+        onPressed: (){},
+      ),
+    );
+  }
+}
+
