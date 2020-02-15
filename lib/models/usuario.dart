d159f19897a6f5487881e36efdca5191384ed1fd
Marcus Camargo <mcamargo@OPP.local>
2020-02-15 19:05:24 -0300

    Usuario Model - Create an id attribute

--- lib/models/usuario.dart
+++ lib/models/usuario.dart
@@ -1,14 +1,15 @@
 class Usuario {
+  final int id;
   final String nome;
   final int numeroConta;
 
   Usuario(
+    this.id,
     this.nome,
     this.numeroConta,
   );
 
 
-
   @override
-  String toString() => 'Usuario nome: $nome, numeroConta: $numeroConta';
+  String toString() => 'Usuario id: $id, nome: $nome, numeroConta: $numeroConta';
 }
