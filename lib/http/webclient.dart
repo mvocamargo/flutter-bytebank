170d5f637aa1b2b1e2dde37e15da67b7bab5b7f8
Marcus Camargo <mvocamargo@gmail.com>
2020-07-25 12:01:42 -0300

    webclient - add save method

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -25,17 +25,21 @@ class LoggingInterceptor implements InterceptorContract {
   }
 }
 
+final Client client = HttpClientWithInterceptor.build(
+  interceptors: [LoggingInterceptor()],
+);
+
+const String baseUrl = 'http://192.168.0.15:8080/transactions';
+
 Future<List<Transferencia>> findAll() async {
-  final Client client = HttpClientWithInterceptor.build(
-    interceptors: [LoggingInterceptor()],
-  );
+  // Improtante setar um timeout para não consumir recursos do dispositivo do usuário "eternamente"
   final Response response =
-      await client.get('http://192.168.0.15:8080/transactions');
+      await client.get(baseUrl).timeout(Duration(seconds: 5));
   final List<dynamic> decodeJson = jsonDecode(response.body);
   final List<Transferencia> transferencias = List();
   for (Map<String, dynamic> transferenciaJson in decodeJson) {
     final Map<String, dynamic> contactJson = transferenciaJson['contact'];
-    final Transferencia transferencia =  Transferencia(
+    final Transferencia transferencia = Transferencia(
       transferenciaJson['value'],
       Usuario(
         0,
@@ -47,4 +51,35 @@ Future<List<Transferencia>> findAll() async {
   }
 
   return transferencias;
+}
+
+Future<Transferencia> save(Transferencia transferencia) async {
+  final Map<String, dynamic> transferenciaMap = {
+    'value': transferencia.valor,
+    'contact': {
+      'name': transferencia.conta.nome,
+      'accountNumber': transferencia.conta.numeroConta,
+    }
+  };
+  final String transferenciaJson = jsonEncode(transferenciaMap);
+
+  final Response response = await client.post(
+    baseUrl,
+    headers: {
+      'Content-type': 'application/json',
+      'password': '1000',
+    },
+    body: transferenciaJson,
+  );
+
+  Map<String, dynamic> json = jsonDecode(response.body);
+  final Map<String, dynamic> contactJson = json['contact'];
+  return Transferencia(
+    json['value'],
+    Usuario(
+      0,
+      contactJson['name'],
+      contactJson['accountNumber'],
+    ),
+  );
 }
