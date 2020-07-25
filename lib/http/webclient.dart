29f0b52c353a0a545d70e55eef6a6ab9d7b60398
Marcus Camargo <mvocamargo@gmail.com>
2020-07-25 10:14:18 -0300

    WebCliente - List all Transferencias

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:bytebank/models/transferencia.dart';
+import 'package:bytebank/models/usuario.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
@@ -6,24 +10,41 @@ class LoggingInterceptor implements InterceptorContract {
   Future<RequestData> interceptRequest({RequestData data}) async {
     print('Request');
     print('url: ${data.baseUrl}');
-    print('body: ${data.headers}');
+    print('headers: ${data.headers}');
     print('body: ${data.body}');
     return data;
   }
 
   @override
   Future<ResponseData> interceptResponse({ResponseData data}) async {
-    print('Request');
+    print('Response');
     print('status code: ${data.statusCode}');
-    print('body: ${data.headers}');
+    print('headers: ${data.headers}');
     print('body: ${data.body}');
     return data;
   }
 }
 
-void findAll() async {
+Future<List<Transferencia>> findAll() async {
   final Client client = HttpClientWithInterceptor.build(
     interceptors: [LoggingInterceptor()],
   );
-  final Response response = await client.get('http://192.168.0.15:8080/transactions');
+  final Response response =
+      await client.get('http://192.168.0.15:8080/transactions');
+  final List<dynamic> decodeJson = jsonDecode(response.body);
+  final List<Transferencia> transferencias = List();
+  for (Map<String, dynamic> transferenciaJson in decodeJson) {
+    final Map<String, dynamic> contactJson = transferenciaJson['contact'];
+    final Transferencia transferencia =  Transferencia(
+      transferenciaJson['valor'],
+      Usuario(
+        0,
+        contactJson['name'],
+        contactJson['accountNumber'],
+      ),
+    );
+    transferencias.add(transferencia);
+  }
+
+  return transferencias;
 }
